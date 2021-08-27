d052f57251bc568a9ab73220218f7822b0cbb8ac
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2021-08-27 16:12:22 +0100

    Added chat functionality between users who have existing conversations, added and implemented react-scroll, suffered a mild headache lol!

--- src/Components/Auth/Chats.js
+++ src/Components/Auth/Chats.js
@@ -1,45 +1,150 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Sidebar from '../Sidebar'
 import '../../Assets/CSS/Dashboard.css'
 import '../../Assets/CSS/Chats.css'
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { animateScroll } from 'react-scroll'
 
 function Chats() {
-    function renderChat(){
-        alert("Chat rendered!")
+    function scrollToBottom(){
+        animateScroll.scrollToBottom({
+            containerId: "chat-area"
+        });
     }
-    function sendMessage(){
-        alert(newMessage)
+    function renderChat(current){
+        console.log(currentContact)
+        setCurrentContact(current)
+        setRenderChatVisibility(true)
+        setTimeout(() => {
+            scrollToBottom();
+        }, 500)
+        
     }
+
+    function getTimeStamp(dateString){
+        var d = new Date(dateString)
+        var hours = d.getHours()
+        var minutes = d.getMinutes()
+
+        if(hours < 10){
+            hours = `0{${hours}}`
+        }
+        if(minutes < 10){
+            minutes = `0${minutes}`;
+        }
+
+        return `${hours}:${minutes}`;
+    }
+    
     const [chatSearch, setChatSearch] = useState('');
+    const [isSearchPeopleShowing, setSearchPeopleShow] = useState(false)
+    const [searchResults, setSearchResults] = useState([])
+
     const [token, setToken] = useState(Cookies.get("ud"))
     const [newMessage, setNewMessage] = useState('');
+
+    
+    const [userDetails, setUserDetails] = useState({})
+    const [messages, setMessages] = useState([])
+    const [isRenderChatVisible, setRenderChatVisibility] = useState(false)
+    const [currentContact, setCurrentContact] = useState({messages: []})
+
+    const chatAreaRef = useRef();
+    const scrollToRef = useRef();
+
     useEffect(() => {
-        axios.post('http://localhost:8080/friends', {id: token})
+        axios.post('http://localhost:8080/user/chats', {id: token})
             .then(response => {
                 console.log(response)
+                setUserDetails(response.data)
             })
             .catch(err => {
                 console.error(err);
             })
     }, [])
+
+    useEffect(() => {
+        if(Object.entries(userDetails).length !== 0){
+            userDetails.messages.map((eachMessage) => {
+                if(Object.entries(eachMessage).length === 0){
+
+                }
+                else{
+                    console.log("A message: ", eachMessage);
+                    axios.post('http://localhost:8080/chat/get', {user_id: eachMessage.id})
+                        .then(response => {
+                            console.log(response)
+                            setMessages(prevMessages => [...prevMessages, {name: response.data.name, email: response.data.email, username: response.data.username, avatar: response.data.avatar, messages: eachMessage.messages, id: eachMessage.id}])
+                        })
+                }
+                
+            })
+        }
+    }, [userDetails])
+
+    useEffect(() => {
+        console.log("Current messages: ", messages)
+    }, [messages])
     useEffect(() => {
         if(chatSearch.length > 0){
             axios.post('http://localhost:8080/users/find', {string: chatSearch})
             .then(res => {
                 console.clear()
                 console.log(res)
+                setSearchResults(res.data)
             })
+        }else{
+            setSearchResults([])
         }
         
     }, [chatSearch])
+
+    function selectChat(contact){
+        console.log(contact)
+        setRenderChatVisibility(true)
+        axios.post('http://localhost:8080/users/get_chat', {username: contact})
+            .then(res => {
+                console.log(res);
+            })
+    }
+
+    function sendMessage(){
+        console.clear()
+        
+        var object = {
+            recipient: currentContact.id,
+            sender: userDetails._id,
+            message: newMessage
+        }
+        console.log(messages)
+        var messageOBJ = {
+            type: "sent",
+            body: newMessage,
+            timestamp: Date.now()
+        }
+        var cm = currentContact.messages
+        cm.push(messageOBJ)
+        setCurrentContact({...currentContact, messages: cm})
+        axios.post('http://localhost:8080/chats/update', object)
+            
+            setNewMessage("")
+            console.clear()
+            
+            setTimeout(() => {
+                var chatArea = document.getElementById("chat-area");
+                var lastMessage = chatArea.lastChild
+                scrollToBottom()
+            }, 100)
+            
+            
+    }
     return (
         <div className="dashboard-container">
             <Sidebar/>
             <div className="dashboard-main">
 
-                <div className="chat-section">
+                <div className="chat-section">                    
                     <div className="contacts">
                         <div className="search-chats">
                             <center>
@@ -50,92 +155,139 @@ function Chats() {
                                     onChange={(e) =>{
                                         setChatSearch(e.target.value)
                                     }}
+                                    onFocus={() => setSearchPeopleShow(true)}
+                                    onBlur={() => setSearchPeopleShow(false)}
                                 />
                             </center>
                         </div>
-                        
+                        <div
+                            className={`search-people ${isSearchPeopleShowing ? "search-people-show" : "search-people-hide"}`}
+                        >
+                            {
+                                searchResults.map(search_result => {
+                                    return(
+                                        <div key={search_result.username}
+                                            className="search-result"
+                                            onClick={() => {
+                                                setCurrentContact(search_result)
+                                                console.log(currentContact)
+                                                selectChat(search_result.username);
+                                            }}
+                                        >
+                                            <img src={search_result.profileURL}
+                                                className="search-result-avatar"
+                                            />
+                                            <span className="search-result-details">
+                                                <span className="search-result-name">
+                                                    {search_result.name}
+                                                </span>
+                                                <span className="search-result-username">
+                                                    @{search_result.username}
+                                                </span>
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                         <div className={`contacts-list`}>
-                            <div className="contact"
-                                onClick={() => {renderChat()}}
-                            >
-                                <div className="contact-properties">
-                                    <img src="https://image.flaticon.com/icons/png/512/3135/3135715.png" alt="" className="contact-image" />
-                                    <div className="contact-details">
-                                        <span className="name">Glad</span>
-                                        <span className="message">
-                                            Where will we go?
-                                        </span>
-                                    </div>
-                                </div>
-                                <span className="time">
-                                    12:45pm
-                                </span>
-                            </div>
-                            <div className="contact"
-                                onClick={() => {renderChat()}}
-                            >
-                                <div className="contact-properties">
-                                    <img src="https://image.flaticon.com/icons/png/512/3135/3135715.png" alt="" className="contact-image" />
-                                    <div className="contact-details">
-                                        <span className="name">Stannis Baratheon</span>
-                                        <span className="message">
-                                            We do not choose our destiny no?
-                                        </span>
-                                    </div>
-                                </div>
-                                <span className="time">
-                                    Yesterday
-                                </span>
-                            </div>
-                            <div className="contact"
-                                onClick={() => {renderChat()}}
-                            >
-                                <div className="contact-properties">
-                                    <img src="https://image.flaticon.com/icons/png/512/3135/3135715.png" alt="" className="contact-image" />
-                                    <div className="contact-details">
-                                        <span className="name">Dr Braavosi</span>
-                                        <span className="message">
-                                            Eat spaghetti, write React 🐇
-                                        </span>
-                                    </div>
-                                </div>
-                                <span className="time">
-                                    11/10/2020
-                                </span>
-                            </div>
+                           
+                            {
+                                messages.map(message => {
+                                    var messageLength = message.messages.length;
+                                    var lastMessage;
+                                    if(messageLength > 0){
+                                        lastMessage = message.messages[messageLength - 1];
+                                    }
+                                    var d = new Date(lastMessage.timestamp)
+                                    var minutes = d.getMinutes()
+                                    var hours = d.getHours()
+                                    if(hours < 10){
+                                        hours = `0{${hours}}`
+                                    }
+                                    if(minutes < 10){
+                                        minutes = `0${minutes}`;
+                                    }
+                                    var timestamp = `${hours}:${minutes}`;
+
+                                    return(
+                                        <div
+                                            key={message.username}
+                                            className="contact"
+                                            onClick={() => renderChat(message)}
+                                        >
+                                            <div className="contact-properties">
+                                                <img src={message.avatar}
+                                                    className="contact-image"
+                                                />
+                                                <div className="contact-details">
+                                                    <span className="name">
+                                                        {message.name}
+                                                    </span>
+                                                    <span className="message">
+                                                        {lastMessage.body}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <span className="time">
+                                                {timestamp}
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
-                    <div className="render-chat">
-                        <span className="current-contact-name">
-                            Dr Braavosi
-                        </span>
-                        <div className="chats-area">
-                            <div className="chat-container received">
-                                <div className="chat received-chat">
-                                    <div className="chat-inside">
-                                        <p className="chat-content">
-                                        This is the exact response I was seeking for. This is the exact response I was seeking for
-                                        </p>
-                                        <div className="chat-timestamp">
-                                            12:23pm
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div className="chat-container sent">
-                                <div className="chat sent-chat">
-                                    <div className="chat-inside">
-                                        <p className="chat-content">
-                                        Why do you think I care?
-                                        </p>
-                                        <span className="chat-timestamp">
-                                            12:23pm
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
+                    <div className={`${isRenderChatVisible ? "render-chat-show" : "render-chat-hide"}`}>
+                        {/* <span className="current-contact-name">
+                            Dr Braavosi
+                        </span> */}
+                        <span className="current-contact-name">
+                            {currentContact.name}
+                        </span>
+                        <div id="chat-area" className="chats-area" ref={chatAreaRef}>
+                            {
+                                currentContact.messages.map(message => {
+                                    if(Object.keys(message).length > 0){
+                                        if(message.type === "received"){
+                                            return(
+                                                <div className="chat-container received">
+                                                    <div className="chat received-chat">
+                                                        <div className="chat-inside">
+                                                            <p className="chat-content">
+                                                                {message.body}
+                                                            </p>
+                                                            <div className="chat-timestamp">
+                                                                {getTimeStamp(message.timestamp)}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }else if(message.type === "sent"){
+                                            return(
+                                                <div className="chat-container sent">
+                                                    <div className="chat sent-chat">
+                                                        <div className="chat-inside">
+                                                            <p className="chat-content">
+                                                                {message.body}
+                                                            </p>
+                                                            <span className="chat-timestamp">
+                                                                {getTimeStamp(message.timestamp)}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+
+                                    }
+                                })
+                            }
+                            
                         </div>
+                            
 
                         <div className="send-message-container">
                             <div className="send-message">
